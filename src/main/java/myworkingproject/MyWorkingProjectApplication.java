0bf389680491e59efebb193b889a94c3d61109aa
Volodymyr Shabanov <volodymyrshw@gmail.com>
2024-04-10 14:58:03 +0200

    first commit

---
+++ src/main/java/myworkingproject/MyWorkingProjectApplication.java
@@ -0,0 +1,13 @@
+package myworkingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWorkingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyWorkingProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/myworkingproject/MyWorkingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package myworkingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWorkingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
